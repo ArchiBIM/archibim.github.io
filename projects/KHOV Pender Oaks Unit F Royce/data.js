825656f764a5b7b04f0e328caf5c2315b4a97acf
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2020-06-09 19:34:43 +0300

    change Roice Unit

--- projects/KHOV Pender Oaks Unit F Royce/data.js
+++ projects/KHOV Pender Oaks Unit F Royce/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-1-living-room",
-      "name": "1 Living Room",
+      "id": "0-unit-f---roice---kitchen-living",
+      "name": "Unit F - Roice - Kitchen-Living",
       "levels": [
         {
           "tileSize": 256,
@@ -20,137 +20,23 @@ var APP_DATA = {
       ],
       "faceSize": 1000,
       "initialViewParameters": {
-        "yaw": 0.7056785470995202,
-        "pitch": -0.020859214985996744,
-        "fov": 1.3046490827601978
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.3256069049455501,
-          "pitch": 0.3070805739617697,
-          "rotation": 0,
-          "target": "1-2-dining---kitchen"
-        },
-        {
-          "yaw": 1.1283055806925475,
-          "pitch": 0.280242200257252,
-          "rotation": 0,
-          "target": "2-3-master-bedroom"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-2-dining---kitchen",
-      "name": "2 Dining - Kitchen",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "yaw": -0.39874014516634304,
-        "pitch": -0.015169687223890449,
-        "fov": 1.3046490827601978
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.8765710909294224,
-          "pitch": 0.34148461513197326,
-          "rotation": 0,
-          "target": "0-1-living-room"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-3-master-bedroom",
-      "name": "3 Master Bedroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "yaw": -2.6424900818718697,
-        "pitch": 0.010342927147382497,
-        "fov": 1.3046490827601978
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.1246583020656225,
-          "pitch": 0.261490464718797,
-          "rotation": 0,
-          "target": "3-4-master-bathroom"
-        },
-        {
-          "yaw": 3.0283803468166637,
-          "pitch": 0.45269262766544927,
-          "rotation": 0,
-          "target": "0-1-living-room"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-4-master-bathroom",
-      "name": "4 Master Bathroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "yaw": 0,
         "pitch": 0,
-        "fov": 1.3046490827601978
+        "yaw": 0,
+        "fov": 1.5707963267948965
       },
       "linkHotspots": [
         {
-          "yaw": -0.4598105671263184,
-          "pitch": 0.48657457216630995,
+          "yaw": 1.1061332180991971,
+          "pitch": 0.253258751258878,
           "rotation": 0,
-          "target": "2-3-master-bedroom"
+          "target": "1-unit-f---roice---foyer-kitchen"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-5-bedroom-2",
-      "name": "5 Bedroom 2",
+      "id": "1-unit-f---roice---foyer-kitchen",
+      "name": "Unit F - Roice - Foyer-Kitchen",
       "levels": [
         {
           "tileSize": 256,
@@ -168,23 +54,47 @@ var APP_DATA = {
       ],
       "faceSize": 1000,
       "initialViewParameters": {
-        "yaw": 1.4941728766647762,
-        "pitch": 0.014427817906575768,
-        "fov": 1.3046490827601978
+        "yaw": -0.05907229897180599,
+        "pitch": -0.000004664053047775951,
+        "fov": 1.4108347438219848
       },
       "linkHotspots": [
         {
-          "yaw": 2.2820038823959683,
-          "pitch": 0.3919297026028019,
+          "yaw": -0.8143628959584408,
+          "pitch": 0.24857499317178266,
           "rotation": 0,
-          "target": "0-1-living-room"
+          "target": "0-unit-f---roice---kitchen-living"
+        },
+        {
+          "yaw": 2.9578981350965243,
+          "pitch": 0.435776200485936,
+          "rotation": 0,
+          "target": "5-unit-f---roice---bedroom-2"
+        },
+        {
+          "yaw": 2.967391563021269,
+          "pitch": 0.1760126582913788,
+          "rotation": 0,
+          "target": "2-unit-f---roice---bathroom-2"
+        },
+        {
+          "yaw": 1.40763441933389,
+          "pitch": 0.14774567592986187,
+          "rotation": 0,
+          "target": "3-unit-f---roice---master-bathroom"
+        },
+        {
+          "yaw": 1.3153116492759142,
+          "pitch": 0.40213399969296937,
+          "rotation": 0,
+          "target": "4-unit-f---roice---master-bedroom"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-6-bathroom-2",
-      "name": "6 Bathroom 2",
+      "id": "2-unit-f---roice---bathroom-2",
+      "name": "Unit F - Roice - Bathroom 2",
       "levels": [
         {
           "tileSize": 256,
@@ -202,32 +112,146 @@ var APP_DATA = {
       ],
       "faceSize": 1000,
       "initialViewParameters": {
-        "yaw": -0.273399135987372,
-        "pitch": 1.0027990014949424,
-        "fov": 1.3046490827601978
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948965
       },
       "linkHotspots": [
         {
-          "yaw": 1.082002480614996,
-          "pitch": 0.3160065992240302,
+          "yaw": 1.1515079384571613,
+          "pitch": 0.2861775398229174,
           "rotation": 0,
-          "target": "0-1-living-room"
+          "target": "1-unit-f---roice---foyer-kitchen"
         },
         {
-          "yaw": 1.2458296979005468,
-          "pitch": 0.26858428694830216,
+          "yaw": 0.7785479398043584,
+          "pitch": 0.6521327099815402,
           "rotation": 0,
-          "target": "1-2-dining---kitchen"
+          "target": "5-unit-f---roice---bedroom-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-unit-f---roice---master-bathroom",
+      "name": "Unit F - Roice - Master Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948965
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4469039205325327,
+          "pitch": 0.44747910786019496,
+          "rotation": 0,
+          "target": "4-unit-f---roice---master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-unit-f---roice---master-bedroom",
+      "name": "Unit F - Roice - Master Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948965
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1103142192911024,
+          "pitch": 0.2620138915947319,
+          "rotation": 0,
+          "target": "3-unit-f---roice---master-bathroom"
+        },
+        {
+          "yaw": -3.0127492890777905,
+          "pitch": 0.3274390748468736,
+          "rotation": 0,
+          "target": "1-unit-f---roice---foyer-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-unit-f---roice---bedroom-2",
+      "name": "Unit F - Roice - Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948965
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0184992254674805,
+          "pitch": 0.12577273631932684,
+          "rotation": 0,
+          "target": "2-unit-f---roice---bathroom-2"
+        },
+        {
+          "yaw": 2.302841821129209,
+          "pitch": 0.23539917027900792,
+          "rotation": 0,
+          "target": "1-unit-f---roice---foyer-kitchen"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "KHOV Pender Oaks Unit F Royce",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
