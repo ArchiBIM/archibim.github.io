85576753f82616c23bba58fc77a218ac08a25d25
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2020-06-18 17:27:23 +0300

    add third floor - r2

--- projects/SMC Paisley - WSG - 3rd floor/data.js
+++ projects/SMC Paisley - WSG - 3rd floor/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-kitchen",
-      "name": "Kitchen",
+      "id": "0-3-rd-fl-kitchen",
+      "name": "3-rd fl. Kitchen",
       "levels": [
         {
           "tileSize": 256,
@@ -24,29 +24,29 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": -1.268551068176233,
-        "pitch": -0.06922988415502118,
+        "yaw": -0.7639314437909004,
+        "pitch": 0.03844793348504183,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.7809071122813691,
-          "pitch": 0.3847916081392171,
+          "yaw": -0.7587564451077551,
+          "pitch": 0.4063421834487908,
           "rotation": 0,
-          "target": "1-breakfast"
+          "target": "1-3-rd-fl-breakfast-room"
         },
         {
-          "yaw": 2.2244799888561264,
-          "pitch": 0.2773813447897915,
+          "yaw": 2.193340887701474,
+          "pitch": 0.29254072127887376,
           "rotation": 0,
-          "target": "2-great-room"
+          "target": "2-3-rd-fl-great-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-breakfast",
-      "name": "Breakfast",
+      "id": "1-3-rd-fl-breakfast-room",
+      "name": "3-rd fl. Breakfast Room",
       "levels": [
         {
           "tileSize": 256,
@@ -68,29 +68,29 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": 0.3361828070179804,
-        "pitch": -0.038895909044445176,
+        "yaw": 0.11127018997910021,
+        "pitch": 0.0014963566517280924,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.5706633498745504,
-          "pitch": 0.16986166042659967,
+          "yaw": 0.8275762890967542,
+          "pitch": 0.4436463177492378,
           "rotation": 0,
-          "target": "2-great-room"
+          "target": "0-3-rd-fl-kitchen"
         },
         {
-          "yaw": 0.8104623176919894,
-          "pitch": 0.43716207359803505,
+          "yaw": -0.3172493828006111,
+          "pitch": 0.22976081189925068,
           "rotation": 0,
-          "target": "0-kitchen"
+          "target": "2-3-rd-fl-great-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-great-room",
-      "name": "Great Room",
+      "id": "2-3-rd-fl-great-room",
+      "name": "3-rd fl. Great Room",
       "levels": [
         {
           "tileSize": 256,
@@ -112,28 +112,28 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": -0.39799558148794745,
-        "pitch": -0.002992847441428026,
+        "yaw": -0.4673388836580976,
+        "pitch": 0.007479982508545646,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -0.8694855703832776,
-          "pitch": 0.35306290924666506,
+          "yaw": -0.0589791495185068,
+          "pitch": 0.22794513773276037,
           "rotation": 0,
-          "target": "0-kitchen"
+          "target": "1-3-rd-fl-breakfast-room"
         },
         {
-          "yaw": 0.03524863521657551,
-          "pitch": 0.195174572286831,
+          "yaw": -0.8983787847063454,
+          "pitch": 0.40004355955607096,
           "rotation": 0,
-          "target": "1-breakfast"
+          "target": "0-3-rd-fl-kitchen"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "SMC Paisley 3-rd floor",
+  "name": "SMC Paisley 3-rd floor R2",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
