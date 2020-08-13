0fc79bae5036305d943f134ed99eead89ad8c2e3
zachdimitrov <zach_dimitrov@yahoo.com>
2020-08-13 17:43:14 +0300

    update Alexis

--- projects/SMC Poland Hill - Alexis/data.js
+++ projects/SMC Poland Hill - Alexis/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-lower-lvl-bathroom",
-      "name": "Lower Lvl Bathroom",
+      "id": "0-1-st-fl-entry",
+      "name": "1-st fl. Entry",
       "levels": [
         {
           "tileSize": 256,
@@ -26,17 +26,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.6565005829065464,
-          "pitch": 1.226447965281178,
+          "yaw": 0.4060963540329965,
+          "pitch": 0.17815192960640758,
           "rotation": 0,
-          "target": "2-lower-rec-room"
+          "target": "3-1-st-fl-living-room"
+        },
+        {
+          "yaw": 0.5374727925967626,
+          "pitch": 0.22953196999439918,
+          "rotation": 0,
+          "target": "1-1-st-fl-kitchen"
+        },
+        {
+          "yaw": 0.11723857100945523,
+          "pitch": 0.5818221935755883,
+          "rotation": 0,
+          "target": "11-lower-lvl-rec-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-lower-lvl-den",
-      "name": "Lower Lvl Den",
+      "id": "1-1-st-fl-kitchen",
+      "name": "1-st fl. Kitchen",
       "levels": [
         {
           "tileSize": 256,
@@ -60,17 +72,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.808852047573648,
-          "pitch": 0.7027927747194198,
+          "yaw": -1.1163410480874703,
+          "pitch": 0.2512192950314258,
           "rotation": 0,
-          "target": "2-lower-rec-room"
+          "target": "3-1-st-fl-living-room"
+        },
+        {
+          "yaw": 0.2006117356342756,
+          "pitch": 0.3121974430814358,
+          "rotation": 0,
+          "target": "2-1-st-fl-eating-room"
+        },
+        {
+          "yaw": -1.5867886615612683,
+          "pitch": -0.15291520048277718,
+          "rotation": 0,
+          "target": "4-2-nd-fl-flex-space"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-lower-rec-room",
-      "name": "Lower Rec Room",
+      "id": "2-1-st-fl-eating-room",
+      "name": "1-st fl. Eating Room",
       "levels": [
         {
           "tileSize": 256,
@@ -94,23 +118,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.055631745985646575,
-          "pitch": 0.5562020013453193,
+          "yaw": 0.6630710724417135,
+          "pitch": 0.1899403155312882,
           "rotation": 0,
-          "target": "0-lower-lvl-bathroom"
+          "target": "0-1-st-fl-entry"
         },
         {
-          "yaw": 0.40154239456343,
-          "pitch": 0.522667927923715,
+          "yaw": -0.23402792803999617,
+          "pitch": 0.3742394515317251,
           "rotation": 0,
-          "target": "1-lower-lvl-den"
+          "target": "1-1-st-fl-kitchen"
+        },
+        {
+          "yaw": 0.8988775873061936,
+          "pitch": -0.1539747001108509,
+          "rotation": 0,
+          "target": "4-2-nd-fl-flex-space"
+        },
+        {
+          "yaw": 1.1214200571733937,
+          "pitch": 0.38023331071477706,
+          "rotation": 0,
+          "target": "3-1-st-fl-living-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-entry",
-      "name": "Entry",
+      "id": "3-1-st-fl-living-room",
+      "name": "1-st fl. Living Room",
       "levels": [
         {
           "tileSize": 256,
@@ -134,23 +170,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.4576658172710104,
-          "pitch": 0.30678674749534984,
+          "yaw": -0.4847313646439151,
+          "pitch": 0.27673411301517703,
           "rotation": 0,
-          "target": "4-living-room"
+          "target": "2-1-st-fl-eating-room"
         },
         {
-          "yaw": 0.3366235187408968,
-          "pitch": 0.6174584137738002,
+          "yaw": 0.3807153525807916,
+          "pitch": 0.24774283783613882,
           "rotation": 0,
-          "target": "7-upper-flex-space"
+          "target": "1-1-st-fl-kitchen"
+        },
+        {
+          "yaw": 1.4718132425015122,
+          "pitch": 0.22328122482231372,
+          "rotation": 0,
+          "target": "0-1-st-fl-entry"
+        },
+        {
+          "yaw": 1.9711263550453246,
+          "pitch": -0.455901173589389,
+          "rotation": 0,
+          "target": "4-2-nd-fl-flex-space"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-living-room",
-      "name": "Living Room",
+      "id": "4-2-nd-fl-flex-space",
+      "name": "2-nd fl. Flex Space",
       "levels": [
         {
           "tileSize": 256,
@@ -174,35 +222,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.4263162519994417,
-          "pitch": 0.3214618407812555,
+          "yaw": -1.6736008194926963,
+          "pitch": 0.3941227569981578,
           "rotation": 0,
-          "target": "5-kitchen"
+          "target": "3-1-st-fl-living-room"
         },
         {
-          "yaw": 1.4620180627690615,
-          "pitch": 0.3024307555452914,
+          "yaw": 0.27154281382796697,
+          "pitch": 0.44519076691477366,
           "rotation": 0,
-          "target": "3-entry"
+          "target": "6-2-nd-fl-owners-suite"
         },
         {
-          "yaw": -0.513821773483425,
-          "pitch": 0.3313772530824721,
+          "yaw": -0.6212916517425082,
+          "pitch": -0.23748590134617942,
           "rotation": 0,
-          "target": "6-eating-room"
-        },
-        {
-          "yaw": 2.0989591116474937,
-          "pitch": 0.375062090597865,
-          "rotation": 0,
-          "target": "1-lower-lvl-den"
+          "target": "7-3-rd-fl-loft"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-kitchen",
-      "name": "Kitchen",
+      "id": "5-2-nd-fl-owners-bathroom",
+      "name": "2-nd fl. Owners Bathroom",
       "levels": [
         {
           "tileSize": 256,
@@ -226,23 +268,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.1689343141000315,
-          "pitch": 0.26079091873978477,
+          "yaw": -0.6571935123104335,
+          "pitch": 0.5338314307557503,
           "rotation": 0,
-          "target": "6-eating-room"
-        },
-        {
-          "yaw": -3.007287396013247,
-          "pitch": 0.5056039042026583,
-          "rotation": 0,
-          "target": "8-owners-suite"
+          "target": "3-1-st-fl-living-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-eating-room",
-      "name": "Eating Room",
+      "id": "6-2-nd-fl-owners-suite",
+      "name": "2-nd fl. Owners Suite",
       "levels": [
         {
           "tileSize": 256,
@@ -266,23 +302,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.1452077522488544,
-          "pitch": 0.3030121540616264,
+          "yaw": 0.6999400606179247,
+          "pitch": 0.5091628139520203,
           "rotation": 0,
-          "target": "4-living-room"
+          "target": "5-2-nd-fl-owners-bathroom"
         },
         {
-          "yaw": -0.27388762843410674,
-          "pitch": 0.43312341244459773,
+          "yaw": 1.0307228276932712,
+          "pitch": 0.42573954712909234,
           "rotation": 0,
-          "target": "5-kitchen"
+          "target": "4-2-nd-fl-flex-space"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-upper-flex-space",
-      "name": "Upper Flex Space",
+      "id": "7-3-rd-fl-loft",
+      "name": "3-rd fl. Loft",
       "levels": [
         {
           "tileSize": 256,
@@ -306,23 +342,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.20146151987892935,
-          "pitch": 0.5431004840080611,
+          "yaw": 0.1982426214752273,
+          "pitch": 0.30786696746605813,
           "rotation": 0,
-          "target": "8-owners-suite"
+          "target": "8-3-rd-fl-office"
         },
         {
-          "yaw": -1.5626192577230071,
-          "pitch": -0.03551200327163606,
+          "yaw": 1.522350800617474,
+          "pitch": 0.5618039431536381,
           "rotation": 0,
-          "target": "10-loft"
+          "target": "9-3-rd-fl-terrace"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-owners-suite",
-      "name": "Owners Suite",
+      "id": "8-3-rd-fl-office",
+      "name": "3-rd fl. Office",
       "levels": [
         {
           "tileSize": 256,
@@ -346,17 +382,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.7524384441069465,
-          "pitch": 0.5606475133295241,
+          "yaw": 0.15896708895861522,
+          "pitch": 0.3545669824145854,
           "rotation": 0,
-          "target": "9-owners-bathroom"
+          "target": "7-3-rd-fl-loft"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-owners-bathroom",
-      "name": "Owners Bathroom",
+      "id": "9-3-rd-fl-terrace",
+      "name": "3-rd fl. Terrace",
       "levels": [
         {
           "tileSize": 256,
@@ -380,57 +416,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.36722834755731,
-          "pitch": 0.8767721612152588,
+          "yaw": 2.809712161989119,
+          "pitch": 0.5249029450085416,
           "rotation": 0,
-          "target": "8-owners-suite"
+          "target": "7-3-rd-fl-loft"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10-loft",
-      "name": "Loft",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.8040090544175555,
-          "pitch": 0.3424982847964735,
-          "rotation": 0,
-          "target": "12-terrace"
-        },
-        {
-          "yaw": 0.28084423577040774,
-          "pitch": 0.3497469806495612,
-          "rotation": 0,
-          "target": "11-office"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "11-office",
-      "name": "Office",
+      "id": "10-lower-lvl-den",
+      "name": "Lower lvl. Den",
       "levels": [
         {
           "tileSize": 256,
@@ -456,8 +452,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "12-terrace",
-      "name": "Terrace",
+      "id": "11-lower-lvl-rec-room",
+      "name": "Lower lvl. Rec. Room",
       "levels": [
         {
           "tileSize": 256,
@@ -479,7 +475,60 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.3522524331198156,
+          "pitch": 0.46123756443955166,
+          "rotation": 0,
+          "target": "10-lower-lvl-den"
+        },
+        {
+          "yaw": 1.494204815382302,
+          "pitch": 0.3189568965868901,
+          "rotation": 0,
+          "target": "0-1-st-fl-entry"
+        },
+        {
+          "yaw": 0.06730202249830342,
+          "pitch": 0.487470170220611,
+          "rotation": 0,
+          "target": "12-lower-lvl-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-lower-lvl-bathroom",
+      "name": "Lower lvl. Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6125263127763105,
+          "pitch": 1.1699969672848987,
+          "rotation": 0,
+          "target": "11-lower-lvl-rec-room"
+        }
+      ],
       "infoHotspots": []
     }
   ],
