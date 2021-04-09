f6c5844ce946f1081e12da024497596f3c9de78b
zachdimitrov <zach_dimitrov@yahoo.com>
2021-04-09 14:22:31 +0300

    replace 20ft

--- projects/KNU District Towns - 20 ft unit/data.js
+++ projects/KNU District Towns - 20 ft unit/data.js
@@ -26,14 +26,14 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.4698102490687948,
-          "pitch": 0.5278664928534162,
+          "yaw": 1.269945495183034,
+          "pitch": 0.4219765483001403,
           "rotation": 0,
           "target": "1-flex-space"
         },
         {
-          "yaw": -0.10273538054978815,
-          "pitch": -0.4170477915475246,
+          "yaw": -0.09425740564327612,
+          "pitch": -0.20734397713819952,
           "rotation": 0,
           "target": "2-kitchen"
         }
@@ -66,10 +66,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.14334763935290873,
-          "pitch": 0.4846463008789872,
+          "yaw": 2.8632445174135075,
+          "pitch": 0.17261889034245925,
           "rotation": 0,
           "target": "0-foyer"
+        },
+        {
+          "yaw": -2.6279004517051128,
+          "pitch": -0.3622729655424184,
+          "rotation": 0,
+          "target": "2-kitchen"
         }
       ],
       "infoHotspots": []
@@ -100,8 +106,14 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.07308657327222079,
-          "pitch": 0.236722280436652,
+          "yaw": 0.39188268738944565,
+          "pitch": 0.31748740110682405,
+          "rotation": 0,
+          "target": "0-foyer"
+        },
+        {
+          "yaw": -0.21094121693369416,
+          "pitch": 0.22660299376303428,
           "rotation": 0,
           "target": "3-living-room"
         }
@@ -134,20 +146,20 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.5630196829748186,
-          "pitch": 0.44599717273302986,
+          "yaw": -0.5358828465610053,
+          "pitch": 0.13045063021579928,
           "rotation": 0,
           "target": "0-foyer"
         },
         {
-          "yaw": 0.3292737893459936,
-          "pitch": 0.29074948749881635,
+          "yaw": 0.1993879537519554,
+          "pitch": 0.2455604403675995,
           "rotation": 0,
           "target": "2-kitchen"
         },
         {
-          "yaw": -2.2727074708462744,
-          "pitch": 0.3794100060135541,
+          "yaw": -1.1042859828765756,
+          "pitch": -0.25547944348775786,
           "rotation": 0,
           "target": "4-owners-bedroom"
         }
@@ -180,14 +192,20 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.746821715341045,
-          "pitch": 0.5000122406403573,
+          "yaw": 1.8964434163904151,
+          "pitch": 0.20833248014977102,
           "rotation": 0,
           "target": "5-owners-bathroom"
         },
         {
-          "yaw": -2.3317120149650066,
-          "pitch": 0.44774385067433364,
+          "yaw": -2.5839400224254945,
+          "pitch": 0.2604484441893433,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": -3.056936118770375,
+          "pitch": 0.14480468049528383,
           "rotation": 0,
           "target": "6-bedroom-2"
         }
@@ -220,8 +238,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.909825843769113,
-          "pitch": 0.6368733375026956,
+          "yaw": 3.1305179913680083,
+          "pitch": 0.6317649505467493,
           "rotation": 0,
           "target": "4-owners-bedroom"
         }
@@ -254,22 +272,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.999515938742549,
-          "pitch": 0.568783787045362,
+          "yaw": -2.5810445701586637,
+          "pitch": 0.044626293796907746,
           "rotation": 0,
           "target": "7-bathroom-2"
         },
         {
-          "yaw": 2.062979433641461,
-          "pitch": -0.20307250723734427,
-          "rotation": 0,
-          "target": "8-loft-1"
-        },
-        {
-          "yaw": 2.1068166632180194,
-          "pitch": 0.46326711266966747,
+          "yaw": 2.1414879975282446,
+          "pitch": 0.38980621557997,
           "rotation": 0,
           "target": "4-owners-bedroom"
+        },
+        {
+          "yaw": 1.9513973976437917,
+          "pitch": -0.2117125870025589,
+          "rotation": 0,
+          "target": "8-loft"
         }
       ],
       "infoHotspots": []
@@ -300,8 +318,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.8564168063126516,
-          "pitch": 0.5151486997922223,
+          "yaw": -2.9958604616945372,
+          "pitch": 0.5598202848887048,
           "rotation": 0,
           "target": "6-bedroom-2"
         }
@@ -309,8 +327,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "8-loft-1",
-      "name": "Loft 1",
+      "id": "8-loft",
+      "name": "Loft",
       "levels": [
         {
           "tileSize": 256,
@@ -334,62 +352,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.21051382477650193,
-          "pitch": 0.5427071155733145,
+          "yaw": 3.00562648095179,
+          "pitch": 0.1818420072635103,
           "rotation": 0,
-          "target": "9-loft-2"
+          "target": "9-rooftop-terrace"
         },
         {
-          "yaw": 1.5734882408179258,
-          "pitch": 0.7642234588965255,
+          "yaw": 2.4992711885856824,
+          "pitch": 0.3666308597116146,
           "rotation": 0,
           "target": "6-bedroom-2"
-        },
-        {
-          "yaw": -3.0753378386524233,
-          "pitch": 0.7396192506974444,
-          "rotation": 0,
-          "target": "10-rooftop-terrace"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-loft-2",
-      "name": "Loft 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.8871544324546701,
-          "pitch": 0.4998312577288946,
-          "rotation": 0,
-          "target": "8-loft-1"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "10-rooftop-terrace",
+      "id": "9-rooftop-terrace",
       "name": "Rooftop Terrace",
       "levels": [
         {
@@ -414,10 +392,10 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.21380155501573306,
-          "pitch": 0.4353412023092069,
+          "yaw": 1.6545890484567654,
+          "pitch": 0.3557648463147238,
           "rotation": 0,
-          "target": "8-loft-1"
+          "target": "8-loft"
         }
       ],
       "infoHotspots": []
@@ -426,8 +404,8 @@ var APP_DATA = {
   "name": "KNU - District Towns_Elv.2 _20'End",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
