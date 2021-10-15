f9633e6aa97737519c0c6340ba3c325d39e1e389
zachdimitrov <zach_dimitrov@yahoo.com>
2021-10-15 21:34:14 +0300

    replace unit A

--- projects/KNU Ashburn Station - Unit A/data.js
+++ projects/KNU Ashburn Station - Unit A/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-kitchen-dining-living",
-      "name": "Kitchen Dining Living",
+      "id": "0-unit-a---kitchenliving",
+      "name": "Unit A - Kitchen/Living",
       "levels": [
         {
           "tileSize": 256,
