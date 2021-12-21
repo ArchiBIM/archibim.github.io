70fd9364c9c096e94552baef640bd3781e3c2684
zachdimitrov <zach_dimitrov@yahoo.com>
2021-12-21 21:54:33 +0200

    replace santee

--- projects/SMC The Santee VR/data.js
+++ projects/SMC The Santee VR/data.js
@@ -26,102 +26,46 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.0716570117537945,
-          "pitch": 0.12303451914707253,
+          "yaw": -0.0596001354559732,
+          "pitch": 0.3595372903218035,
           "rotation": 0,
-          "target": "7-2-nd-fl-bonus-room"
+          "target": "1-1-st-fl-breakfast-area"
         },
         {
-          "yaw": -0.1153779386252225,
-          "pitch": 0.290638918417109,
+          "yaw": 1.0503126532660438,
+          "pitch": 0.18564280170881808,
           "rotation": 0,
-          "target": "3-1-st-fl-breakfast-area"
+          "target": "9-2-nd-fl-bonus-room"
+        },
+        {
+          "yaw": -1.3254563095924041,
+          "pitch": 0.6738923764407758,
+          "rotation": 0,
+          "target": "7-1-st-fl-bathroom2"
+        },
+        {
+          "yaw": -1.6331285985719592,
+          "pitch": 0.41459565479741656,
+          "rotation": 0,
+          "target": "8-1-st-fl-bedroom3"
+        },
+        {
+          "yaw": -2.0317611467395214,
+          "pitch": 0.6572683935781072,
+          "rotation": 0,
+          "target": "6-1-st-fl-bedroom2"
+        },
+        {
+          "yaw": 0.09624581416500178,
+          "pitch": 0.2214228566706815,
+          "rotation": 0,
+          "target": "4-1-st-fl-owners-bedroom"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-1-st-fl-bedroom2",
-      "name": "1-st fl. Bedroom2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.9302244614182236,
-          "pitch": 0.6172710321149033,
-          "rotation": 0,
-          "target": "0-1-st-fl-foyer"
-        },
-        {
-          "yaw": 2.687399669750711,
-          "pitch": 0.5098163918354217,
-          "rotation": 0,
-          "target": "8-1-fl-owners-bath"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-1-st-fl-bedroom3",
-      "name": "1-st fl. Bedroom3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.027233865019878,
-          "pitch": 0.468500936485043,
-          "rotation": 0,
-          "target": "0-1-st-fl-foyer"
-        },
-        {
-          "yaw": 2.7055458373165795,
-          "pitch": 0.03692246683199585,
-          "rotation": 0,
-          "target": "7-2-nd-fl-bonus-room"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-1-st-fl-breakfast-area",
+      "id": "1-1-st-fl-breakfast-area",
       "name": "1-st fl. Breakfast area",
       "levels": [
         {
@@ -146,22 +90,28 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.10625147254187972,
-          "pitch": 0.31591247917370424,
+          "yaw": 0.8721463344559055,
+          "pitch": 0.5396491487330994,
           "rotation": 0,
-          "target": "4-1-st-fl-family-room"
+          "target": "3-1-st-fl-kitchen"
         },
         {
-          "yaw": -0.650262363957852,
-          "pitch": 0.32748550722899594,
+          "yaw": -0.29453705947938325,
+          "pitch": 0.33547098708817025,
           "rotation": 0,
-          "target": "3-1-st-fl-breakfast-area"
+          "target": "2-1-st-fl-family-room"
+        },
+        {
+          "yaw": -0.6334515401712189,
+          "pitch": 0.25082190397916015,
+          "rotation": 0,
+          "target": "4-1-st-fl-owners-bedroom"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-1-st-fl-family-room",
+      "id": "2-1-st-fl-family-room",
       "name": "1-st fl. Family Room",
       "levels": [
         {
@@ -186,28 +136,34 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.8959875848549856,
-          "pitch": 0.444038390123886,
-          "rotation": 0,
-          "target": "4-1-st-fl-family-room"
-        },
-        {
-          "yaw": -0.7301591236039595,
-          "pitch": 0.2571853757682412,
+          "yaw": -0.7249492327151188,
+          "pitch": 0.2647984962902825,
           "rotation": 0,
           "target": "0-1-st-fl-foyer"
         },
         {
-          "yaw": 0.4787673846225413,
-          "pitch": 0.3291357885509534,
+          "yaw": 0.5921556210767811,
+          "pitch": 0.35401763912842554,
           "rotation": 0,
-          "target": "5-1-st-fl-kitchen"
+          "target": "1-1-st-fl-breakfast-area"
+        },
+        {
+          "yaw": 0.011107708612989597,
+          "pitch": 0.39964614107696406,
+          "rotation": 0,
+          "target": "3-1-st-fl-kitchen"
+        },
+        {
+          "yaw": -2.4452444102111865,
+          "pitch": 0.6455427476976823,
+          "rotation": 0,
+          "target": "4-1-st-fl-owners-bedroom"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-1-st-fl-kitchen",
+      "id": "3-1-st-fl-kitchen",
       "name": "1-st fl. Kitchen",
       "levels": [
         {
@@ -232,22 +188,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.4624619916642025,
-          "pitch": 0.5422924864401253,
+          "yaw": 2.827074005448564,
+          "pitch": 0.3488257715093823,
           "rotation": 0,
-          "target": "4-1-st-fl-family-room"
+          "target": "4-1-st-fl-owners-bedroom"
         },
         {
-          "yaw": 2.7551858643564557,
-          "pitch": 0.4216002220333994,
+          "yaw": -1.8742918221212044,
+          "pitch": 0.45985604041560535,
           "rotation": 0,
-          "target": "3-1-st-fl-breakfast-area"
+          "target": "0-1-st-fl-foyer"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-1-st-fl-owners-bedroom",
+      "id": "4-1-st-fl-owners-bedroom",
       "name": "1-st fl. Owner's Bedroom",
       "levels": [
         {
@@ -272,50 +228,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.0967829316142668,
-          "pitch": 0.26788628816641236,
+          "yaw": 2.203833012297652,
+          "pitch": 0.25680797199100347,
           "rotation": 0,
-          "target": "5-1-st-fl-kitchen"
+          "target": "1-1-st-fl-breakfast-area"
+        },
+        {
+          "yaw": 0.1452461764692572,
+          "pitch": 0.411105464761075,
+          "rotation": 0,
+          "target": "5-1-fl-owners-bath"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-2-nd-fl-bonus-room",
-      "name": "2-nd fl. Bonus room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.7943580453794983,
-          "pitch": 0.6744974231332996,
-          "rotation": 0,
-          "target": "0-1-st-fl-foyer"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "8-1-fl-owners-bath",
+      "id": "5-1-fl-owners-bath",
       "name": "1 fl. Owner's Bath",
       "levels": [
         {
@@ -340,16 +268,170 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.7309836504359026,
-          "pitch": 0.3904185934355606,
+          "yaw": 0.01440229050434283,
+          "pitch": 0.4436598582255016,
           "rotation": 0,
-          "target": "6-1-st-fl-owners-bedroom"
+          "target": "4-1-st-fl-owners-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-1-st-fl-bedroom2",
+      "name": "1-st fl. Bedroom2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7030792798062446,
+          "pitch": 0.5939409977026955,
+          "rotation": 0,
+          "target": "7-1-st-fl-bathroom2"
+        },
+        {
+          "yaw": -2.9479120179076865,
+          "pitch": 0.6157000722162582,
+          "rotation": 0,
+          "target": "0-1-st-fl-foyer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-1-st-fl-bathroom2",
+      "name": "1-st fl. Bathroom2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.49950348245145015,
+          "pitch": 0.5613471706243001,
+          "rotation": 0,
+          "target": "0-1-st-fl-foyer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-1-st-fl-bedroom3",
+      "name": "1-st fl. Bedroom3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9271709195502176,
+          "pitch": 0.7313059333077554,
+          "rotation": 0,
+          "target": "6-1-st-fl-bedroom2"
+        },
+        {
+          "yaw": 3.0860632138953212,
+          "pitch": 0.47574494385855814,
+          "rotation": 0,
+          "target": "0-1-st-fl-foyer"
+        },
+        {
+          "yaw": 2.7068920432490104,
+          "pitch": 0.03843276385393324,
+          "rotation": 0,
+          "target": "9-2-nd-fl-bonus-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-2-nd-fl-bonus-room",
+      "name": "2-nd fl. Bonus room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1435165057170504,
+          "pitch": 0.7566467091627676,
+          "rotation": 0,
+          "target": "0-1-st-fl-foyer"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "SMC Santee VR",
+  "name": "SMC The Santee VR",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
