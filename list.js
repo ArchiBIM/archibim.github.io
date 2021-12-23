210989d9c6bbb65ea9d47be7350223ee52b3b372
zachdimitrov <zach_dimitrov@yahoo.com>
2021-12-23 18:42:34 +0200

    Cleanup and add Blanche

--- list.js
+++ list.js
@@ -1,35 +1,41 @@
 var data = {
     clients: [{
+            name: "SMC Blanche",
+            dirs: [
+                "SMC ATL Visualizer - Blanche"
+            ]
+        },
+        {
             name: "SMC",
             dirs: [
-                "SMC Paisley - WSG - VR",
-                "SMC Paisley - WSG - Clay 1",
-				"SMC Paisley - WSG - Clay 2",
-				"SMC Magnolia Walk Amenity",
-				"SMC Poland Hill - Eva",
-				"SMC Poland Hill - Eva Basement",
-				"SMC Poland Hill - Woodward",
-				"SMC Poland Hill - Wayne",
-				"SMC Poland Hill - Alexis",
-				"SMC Tall Oaks - D55 - A",
-				"SMC Potomac Shores - The Maya",
-				"SMC Riverfront at West Hyattsville - Wagner",
-				"SMC Riverfront at West Hyattsville - Kelly",
-				"SMC RIC RK2 The Reuben VR",
-				"SMC RIC RK2 The Bethany VR",
-				"SMC CVL BRH - The Beck",
-				"SMC Delilah VR Tour",
-				"SMC The Dana A",
-				"SMC The Dana B end",
-				"SMC The Beck - Changes",
-				"SMC Cabin Branch - Avenlee F",
-				"SMC Cabin Branch - Oberon D",
-				"SMC Cabin Branch - Landry D",
-				"SMC Retreat at One",
-				"SMC RAO - Balfour H End",
-				"SMC ATL Blanche - Living Room",
-				"SMC ATL Blanche - Master Bath",
-				"SMC The Santee VR"
+                //"SMC Paisley - WSG - VR",
+                //"SMC Paisley - WSG - Clay 1",
+                //"SMC Paisley - WSG - Clay 2",
+                //"SMC Magnolia Walk Amenity",
+                //"SMC Poland Hill - Eva",
+                "SMC Poland Hill - Eva Basement",
+                //"SMC Poland Hill - Woodward",
+                //"SMC Poland Hill - Wayne",
+                //"SMC Poland Hill - Alexis",
+                //"SMC Tall Oaks - D55 - A",
+                //"SMC Potomac Shores - The Maya",
+                //"SMC Riverfront at West Hyattsville - Wagner",
+                //"SMC Riverfront at West Hyattsville - Kelly",
+                //"SMC RIC RK2 The Reuben VR",
+                //"SMC RIC RK2 The Bethany VR",
+                //"SMC CVL BRH - The Beck",
+                "SMC Delilah VR Tour",
+                "SMC The Dana A",
+                "SMC The Dana B end",
+                "SMC The Beck - Changes",
+                "SMC Cabin Branch - Avenlee F",
+                "SMC Cabin Branch - Oberon D",
+                "SMC Cabin Branch - Landry D",
+                "SMC Retreat at One",
+                "SMC RAO - Balfour H End",
+                "SMC ATL Blanche - Living Room",
+                "SMC ATL Blanche - Master Bath",
+                "SMC The Santee VR"
             ]
         },
         {
@@ -41,49 +47,49 @@ var data = {
         {
             name: "KHOV",
             dirs: [
-				//"KHOV Pender Oaks Unit F Royce Color",
-                "KHOV Pender Oaks Unit F Royce",
-				//"KHOV Pender Oaks Unit B Copeland",
-				"KHOV Pender Oaks Unit C Dunbar",
-				"KHOV Pender Oaks Unit D Evert",
-				"KHOV Pender Oaks Unit E Larkin",
+                //"KHOV Pender Oaks Unit F Royce Color",
+                //"KHOV Pender Oaks Unit F Royce",
+                //"KHOV Pender Oaks Unit B Copeland",
+                //"KHOV Pender Oaks Unit C Dunbar",
+                //"KHOV Pender Oaks Unit D Evert",
+                //"KHOV Pender Oaks Unit E Larkin",
             ]
         },
-		{
-			name: "EYA",
-			dirs: [
-			    "EYA Midline - Reston - Unit А",
-				"EYA Midline - Reston - Unit B",
-				"EYA Midline - Reston - Unit C",
-				"EYA Midline - Reston - Unit A - R2",
-				"EYA Midline - Reston - Unit B - R2",
-				"EYA Midline - Reston - Unit C - R2",
-				"EYA Sutton Heights - Unit B2",
-				"EYA Sutton Heights - Unit C1"
-			]
-		},
-		{
-			name: "FSC",
-			dirs: [
-			"FSC Dylan - Unit A3",
-			"FSC Dylan - Unit B1",
-			"FSC Dylan - Unit C5",
-			"FSC Dylan - Unit E1",
-			]
-		},
-		{
-			name: "KNU",
-			dirs: [
-			"KNU Reston Faraday VR Tour",
-			"KNU Faraday VR Tour",
-			"KNU District Towns - 20 ft unit",
-			"KNU District Towns - 16 ft unit",
-			"KNU Birchwood B1",
-			"KNU Birchwood D3",
-			"KNU Ashburn Station - Unit A",
-			"KNU Ashburn Station - Unit C",
-			"KNU Birchwood Exterior"
-			]
-		}
+        {
+            name: "EYA",
+            dirs: [
+                "EYA Midline - Reston - Unit А",
+                "EYA Midline - Reston - Unit B",
+                "EYA Midline - Reston - Unit C",
+                "EYA Midline - Reston - Unit A - R2",
+                "EYA Midline - Reston - Unit B - R2",
+                "EYA Midline - Reston - Unit C - R2",
+                "EYA Sutton Heights - Unit B2",
+                "EYA Sutton Heights - Unit C1"
+            ]
+        },
+        {
+            name: "FSC",
+            dirs: [
+                "FSC Dylan - Unit A3",
+                "FSC Dylan - Unit B1",
+                "FSC Dylan - Unit C5",
+                "FSC Dylan - Unit E1",
+            ]
+        },
+        {
+            name: "KNU",
+            dirs: [
+                //"KNU Reston Faraday VR Tour",
+                //"KNU Faraday VR Tour",
+                "KNU District Towns - 20 ft unit",
+                "KNU District Towns - 16 ft unit",
+                "KNU Birchwood B1",
+                "KNU Birchwood D3",
+                "KNU Ashburn Station - Unit A",
+                "KNU Ashburn Station - Unit C",
+                "KNU Birchwood Exterior"
+            ]
+        }
     ]
 }
