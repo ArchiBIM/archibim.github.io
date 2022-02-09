7a1eb098c08a2bd6cfacd22585ab036e625721b6
zachdimitrov <zach_dimitrov@yahoo.com>
2022-02-09 14:35:00 +0200

    add list 2

--- list.js
+++ list.js
@@ -91,12 +91,12 @@ var data = {
                 "KNU Birchwood Exterior"
             ]
         },
-	{
-		name: "KNU",
-		dirs: [
-		"BBH-2201 The Heron - Package 1",
-		"BBH-2201 The Heron - Package 2"
-		]
-	}
+		{
+			name: "KNU",
+			dirs: [
+				"BBH-2201 The Heron - Package 1",
+				"BBH-2201 The Heron - Package 2"
+			]
+		}
     ]
 }
