c2b05e5ecbe562a563a74a0f08f71a4384fe6be8
zachdimitrov <zach_dimitrov@yahoo.com>
2022-03-18 19:35:30 +0200

    add Maisie

--- projects/SMC Maisie II Visualizer/data.js
+++ projects/SMC Maisie II Visualizer/data.js
@@ -64,8 +64,41 @@ var APP_DATA = {
           "pitch": 0.3789288449659445,
           "rotation": 0,
           "target": "0-family"
+        },
+        {
+          "yaw": -2.5368777972973646,
+          "pitch": -0.027192481277248604,
+          "rotation": 0,
+          "target": "2-owners-bath"
         }
       ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-owners-bath",
+      "name": "Owners Bath",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
