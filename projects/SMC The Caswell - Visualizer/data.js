d65a736e7338e41475635068d279d688a026ad79
zachdimitrov <zach_dimitrov@yahoo.com>
2022-04-12 17:51:28 +0300

    replace caswell

--- projects/SMC The Caswell - Visualizer/data.js
+++ projects/SMC The Caswell - Visualizer/data.js
@@ -1,54 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-family-room",
-      "name": "Family Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.35194771555931936,
-          "pitch": 0.24174050352455367,
-          "rotation": 0,
-          "target": "1-kitchen-2"
-        },
-        {
-          "yaw": -0.8044984898195437,
-          "pitch": 0.2713481950606287,
-          "rotation": 0,
-          "target": "2-kitchen-1"
-        },
-        {
-          "yaw": 0.8124468314487974,
-          "pitch": 0.24138768557133794,
-          "rotation": 0,
-          "target": "3-owners-bath"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-kitchen-2",
-      "name": "Kitchen 2",
+      "id": "0-kitchen-living-1",
+      "name": "Kitchen-Living 1",
       "levels": [
         {
           "tileSize": 256,
@@ -72,29 +26,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.2287056373140555,
-          "pitch": 0.6025447147149041,
+          "yaw": 1.5386203322429282,
+          "pitch": 0.31738317869601396,
           "rotation": 0,
-          "target": "2-kitchen-1"
+          "target": "1-kitchen-living-2"
         },
         {
-          "yaw": 2.4579666407881673,
-          "pitch": 0.07215213876730253,
+          "yaw": 2.070701700595073,
+          "pitch": -0.05244607888630881,
           "rotation": 0,
-          "target": "0-family-room"
-        },
-        {
-          "yaw": 1.7464674711756523,
-          "pitch": -0.14024469198815126,
-          "rotation": 0,
-          "target": "3-owners-bath"
+          "target": "2-owners-bath"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-kitchen-1",
-      "name": "Kitchen 1",
+      "id": "1-kitchen-living-2",
+      "name": "Kitchen-Living 2",
       "levels": [
         {
           "tileSize": 256,
@@ -110,7 +58,7 @@ var APP_DATA = {
           "size": 1024
         }
       ],
-      "faceSize": 1000,
+      "faceSize": 1024,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -118,28 +66,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.11958911340108713,
-          "pitch": 0.4858588335959819,
+          "yaw": -1.514642362210246,
+          "pitch": 0.3512981198664047,
           "rotation": 0,
-          "target": "1-kitchen-2"
+          "target": "1-kitchen-living-2"
         },
         {
-          "yaw": 0.6438131382193326,
-          "pitch": -0.1041567827591976,
+          "yaw": 2.226249478996964,
+          "pitch": -0.26061122025346073,
           "rotation": 0,
-          "target": "3-owners-bath"
-        },
-        {
-          "yaw": 0.969307639056403,
-          "pitch": 0.2642491871071968,
-          "rotation": 0,
-          "target": "0-family-room"
+          "target": "2-owners-bath"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-owners-bath",
+      "id": "2-owners-bath",
       "name": "Owners Bath",
       "levels": [
         {
@@ -162,14 +104,7 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -1.6673462891196031,
-          "pitch": 0.5172280570822636,
-          "rotation": 0,
-          "target": "0-family-room"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
