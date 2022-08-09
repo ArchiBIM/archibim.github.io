b273689997ef4eb6dbf2cde247e2fab36dcf0ab5
zachdimitrov <zach_dimitrov@yahoo.com>
2022-08-09 20:30:38 +0300

    replace Wanda

--- projects/SMC The Wanda - Visualizer/data.js
+++ projects/SMC The Wanda - Visualizer/data.js
@@ -1,8 +1,35 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-smc---the-wanda---premier-a",
-      "name": "SMC - The Wanda - Premier A",
+      "id": "0-living-kitchen",
+      "name": "Living-Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-owners-bathroom",
+      "name": "Owners Bathroom",
       "levels": [
         {
           "tileSize": 256,
