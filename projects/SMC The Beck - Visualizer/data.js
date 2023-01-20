3da3f5091e24865e198852ea11b812de30f34a39
zachdimitrov <zach_dimitrov@yahoo.com>
2023-01-20 20:42:05 +0200

    Update the Beck

--- projects/SMC The Beck - Visualizer/data.js
+++ projects/SMC The Beck - Visualizer/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-eating--kitchen",
-      "name": "Eating / Kitchen",
+      "id": "0-interior---tailored-c",
+      "name": "Interior - Tailored C",
       "levels": [
         {
           "tileSize": 256,
@@ -28,8 +28,62 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "1-primary-bath",
-      "name": "Primary Bath",
+      "id": "1-interior---premier-d",
+      "name": "Interior - Premier D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bath---classic-a",
+      "name": "Bath - Classic A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bath---taylored-c",
+      "name": "Bath - Taylored C",
       "levels": [
         {
           "tileSize": 256,
@@ -55,7 +109,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "The Beck - Visualizer",
+  "name": "SMC The Beck - Visualizer",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
