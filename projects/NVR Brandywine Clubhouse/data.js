b5f5e6ef4d8af8473eebd95cdfa15258ec1f9f19
zachdimitrov <zach_dimitrov@yahoo.com>
2024-05-23 19:34:09 +0300

    Replace Brandywine

--- projects/NVR Brandywine Clubhouse/data.js
+++ projects/NVR Brandywine Clubhouse/data.js
@@ -35,7 +35,7 @@ var APP_DATA = {
           "yaw": 0.4390374446636738,
           "pitch": 0.3320587684746368,
           "rotation": 0,
-          "target": "2-fireplase"
+          "target": "2-fireplace"
         },
         {
           "yaw": -0.14736934582358074,
@@ -81,8 +81,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-fireplase",
-      "name": "Fireplase",
+      "id": "2-fireplace",
+      "name": "Fireplace",
       "levels": [
         {
           "tileSize": 256,
@@ -185,7 +185,7 @@ var APP_DATA = {
           "yaw": -1.5452578081232744,
           "pitch": 0.29969261272972325,
           "rotation": 0,
-          "target": "2-fireplase"
+          "target": "2-fireplace"
         },
         {
           "yaw": -2.651341036446203,
@@ -231,7 +231,7 @@ var APP_DATA = {
           "yaw": -0.8181373644985666,
           "pitch": 0.1129073029379537,
           "rotation": 0,
-          "target": "2-fireplase"
+          "target": "2-fireplace"
         }
       ],
       "infoHotspots": []
@@ -265,7 +265,7 @@ var APP_DATA = {
           "yaw": -3.03088742158973,
           "pitch": 0.3293837495238705,
           "rotation": 0,
-          "target": "2-fireplase"
+          "target": "2-fireplace"
         }
       ],
       "infoHotspots": []
@@ -299,7 +299,7 @@ var APP_DATA = {
           "yaw": 1.9834084505361034,
           "pitch": 0.05243200189242714,
           "rotation": 0,
-          "target": "2-fireplase"
+          "target": "2-fireplace"
         }
       ],
       "infoHotspots": []
@@ -333,14 +333,14 @@ var APP_DATA = {
           "yaw": -3.0417182508757303,
           "pitch": 0.21891271842720172,
           "rotation": 0,
-          "target": "2-fireplase"
+          "target": "2-fireplace"
         }
       ],
       "infoHotspots": []
     },
     {
       "id": "8-covered-patio",
-      "name": "Covered patio",
+      "name": "Covered Patio",
       "levels": [
         {
           "tileSize": 256,
@@ -356,7 +356,7 @@ var APP_DATA = {
           "size": 1024
         }
       ],
-      "faceSize": 1000,
+      "faceSize": 1024,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -364,22 +364,212 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.664527342951823,
-          "pitch": 0.15223870859233735,
+          "yaw": -2.7482314273557495,
+          "pitch": 0.41702034879847716,
           "rotation": 0,
           "target": "3-kitchen"
         },
         {
-          "yaw": -1.2133529024265783,
-          "pitch": 0.02241263354514267,
+          "yaw": 3.049781809353072,
+          "pitch": 0.2466364330583346,
           "rotation": 0,
-          "target": "4-recreation-room"
+          "target": "2-fireplace"
         },
         {
-          "yaw": -0.13806364735878596,
-          "pitch": 0.16279852472751521,
+          "yaw": -0.0029074999683942337,
+          "pitch": 0.3863609463269704,
           "rotation": 0,
-          "target": "2-fireplase"
+          "target": "9-fire-pit-seating-area"
+        },
+        {
+          "yaw": -1.0813152629019953,
+          "pitch": 0.1901631987637291,
+          "rotation": 0,
+          "target": "10-seating-area-with-planting-bed"
+        },
+        {
+          "yaw": -0.40139728055628154,
+          "pitch": 0.0479315750794882,
+          "rotation": 0,
+          "target": "11-pool-and-hot-tub"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-fire-pit-seating-area",
+      "name": "Fire pit seating area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.838647238709868,
+          "pitch": 0.4017900191901269,
+          "rotation": 0,
+          "target": "8-covered-patio"
+        },
+        {
+          "yaw": -2.8263450722026384,
+          "pitch": 0.19002287667557027,
+          "rotation": 0,
+          "target": "3-kitchen"
+        },
+        {
+          "yaw": -1.4828541435209779,
+          "pitch": 0.1673011242839415,
+          "rotation": 0,
+          "target": "10-seating-area-with-planting-bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-seating-area-with-planting-bed",
+      "name": "Seating area with planting bed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9665773274004374,
+          "pitch": 0.1719543872250675,
+          "rotation": 0,
+          "target": "8-covered-patio"
+        },
+        {
+          "yaw": 1.4277018074411583,
+          "pitch": 0.1712273065503087,
+          "rotation": 0,
+          "target": "9-fire-pit-seating-area"
+        },
+        {
+          "yaw": 0.016855351514408312,
+          "pitch": 0.08964999103552884,
+          "rotation": 0,
+          "target": "11-pool-and-hot-tub"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pool-and-hot-tub",
+      "name": "Pool and hot tub",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6404228372433654,
+          "pitch": 0.22398224195117677,
+          "rotation": 0,
+          "target": "10-seating-area-with-planting-bed"
+        },
+        {
+          "yaw": 2.184709210058166,
+          "pitch": 0.1312270323291873,
+          "rotation": 0,
+          "target": "9-fire-pit-seating-area"
+        },
+        {
+          "yaw": 0.22409688011965834,
+          "pitch": 0.03461487966647603,
+          "rotation": 0,
+          "target": "12-patio-bocce-court-and-pickelball-courts"
+        },
+        {
+          "yaw": 2.4430966580686855,
+          "pitch": 0.02815509451736986,
+          "rotation": 0,
+          "target": "8-covered-patio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-patio-bocce-court-and-pickelball-courts",
+      "name": "Patio, bocce court and pickelball courts",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1384542016529657,
+          "pitch": 0.06113963445105952,
+          "rotation": 0,
+          "target": "11-pool-and-hot-tub"
         }
       ],
       "infoHotspots": []
