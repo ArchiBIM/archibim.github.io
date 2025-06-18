292a3bc015fe22337e14cdb65b69175e9e2da415
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2025-06-18 16:30:29 +0300

    Replace Fairchild

--- projects/SMC_The-Fairchild_Color-Preview/data.js
+++ projects/SMC_The-Fairchild_Color-Preview/data.js
@@ -24,16 +24,41 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 2.284689835780295,
-        "pitch": -0.023690645662060916,
-        "fov": 1.4083983627516938
+        "yaw": 2.502710244560779,
+        "pitch": -0.0016012194972567784,
+        "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.1655220135070632,
+          "pitch": 0.8264646660499615,
+          "rotation": 0,
+          "target": "2-living---platinum-a"
+        },
+        {
+          "yaw": -3.1318361102054215,
+          "pitch": 0.2670790180727636,
+          "rotation": 0,
+          "target": "1-dining---platinum-a"
+        },
+        {
+          "yaw": -2.3158586995971895,
+          "pitch": 0.8136226892067686,
+          "rotation": 0,
+          "target": "4-bathroom---platinum-a"
+        },
+        {
+          "yaw": -2.762041357837166,
+          "pitch": 0.5171897422806069,
+          "rotation": 0,
+          "target": "3-bedroom"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "1-bath---platinum-a",
-      "name": "Bath - Platinum A",
+      "id": "1-dining---platinum-a",
+      "name": "Dining - Platinum A",
       "levels": [
         {
           "tileSize": 256,
@@ -55,78 +80,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -1.4955885026635105,
-        "pitch": 0.040475889292409306,
-        "fov": 1.4083983627516938
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-kitchen---platinum-b",
-      "name": "Kitchen - Platinum B",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 2.3677678851336763,
-        "pitch": 0.015937947041777534,
-        "fov": 1.4083983627516938
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-bath---platinum-b",
-      "name": "Bath - Platinum B",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": 3.058270096030939,
+        "pitch": 0.4675560931948226,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "4-kitchen---platinum-c",
-      "name": "Kitchen - Platinum C",
+      "id": "2-living---platinum-a",
+      "name": "Living - Platinum A",
       "levels": [
         {
           "tileSize": 256,
@@ -148,16 +111,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": 0.9954253957073362,
+        "pitch": 0.16822003214369197,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "5-bath---platinum-c",
-      "name": "Bath - Platinum C",
+      "id": "3-bedroom",
+      "name": "Bedroom",
       "levels": [
         {
           "tileSize": 256,
@@ -179,16 +142,16 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": -0.6952791262111191,
+        "pitch": 0.17133048620495472,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "6-kitchen---platinum-d",
-      "name": "Kitchen - Platinum D",
+      "id": "4-bathroom---platinum-a",
+      "name": "Bathroom - Platinum A",
       "levels": [
         {
           "tileSize": 256,
@@ -210,178 +173,15 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 2.873129281373936,
-        "pitch": -0.006526405758810583,
-        "fov": 1.4083983627516938
+        "yaw": -1.6888506741547502,
+        "pitch": 0.17533353494806292,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "7-bath---platinum-d",
-      "name": "Bath - Platinum D",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -2.1391569260745307,
-        "pitch": 0.14643303875342362,
-        "fov": 1.4083983627516938
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "8-tour-dining-room",
-      "name": "Tour Dining Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.47599888690754355,
-        "pitch": -0.006526405758810583,
-        "fov": 1.4083983627516938
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.03273261372298286,
-          "pitch": 0.010334330694622196,
-          "rotation": 0,
-          "target": "9-tour-living-room"
-        },
-        {
-          "yaw": -1.3135912278799005,
-          "pitch": 0.43551728839675974,
-          "rotation": 0,
-          "target": "10-tour-bedroom"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "9-tour-living-room",
-      "name": "Tour Living Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 1.2211332140749445,
-        "pitch": 0.05472966133600643,
-        "fov": 1.4083983627516938
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.839920402174407,
-          "pitch": -0.01753237655723261,
-          "rotation": 0,
-          "target": "8-tour-dining-room"
-        },
-        {
-          "yaw": 3.0566004184521898,
-          "pitch": 0.2039075289470471,
-          "rotation": 0,
-          "target": "10-tour-bedroom"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "10-tour-bedroom",
-      "name": "Tour Bedroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.5312147577888169,
-        "pitch": 0.01435809266938648,
-        "fov": 1.4083983627516938
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.7655007130754914,
-          "pitch": 0.07571780849319154,
-          "rotation": 0,
-          "target": "9-tour-living-room"
-        },
-        {
-          "yaw": -2.4853537206236105,
-          "pitch": 0.07177378733953788,
-          "rotation": 0,
-          "target": "8-tour-dining-room"
-        }
-      ],
       "infoHotspots": []
     }
   ],
-  "name": "The Fairchild",
+  "name": "Fairchild",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
